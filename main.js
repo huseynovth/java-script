384a823979b280d3ef3f4552a21ff2ea3b937366
Tahir Huseynov <126083017+huseynovth@users.noreply.github.com>
2023-05-02 11:36:41 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// let num=2312;
+// if (num%2){
+//     console.log("tek ededdir");
+// }
+// else  if(num==0){
+//     console.log("eded 0-'dir");
+// }
+// else{
+//     console.log("cut ededdir");
+// }
+
+
+
+// function  myfunc(a,b,c){
+//     return a+b+c
+// }
+// console.log(myfunc(2,3,4));
+
+// function myfunc(min,max){
+//     if(min<max){
+//         var result=0
+//         for (let index=min;index<=max;index++){
+//             result+=index
+//         }
+//         return result
+//     }
+//     return 'invalid input'
+// }
+// console.log(myfunc(5,2))
